d66b4cb48a7784e98d908282a8336b70b60b0dfd
skywarth <yigitk.ersoy@gmail.com>
2020-01-25 14:07:45 +0400

    jquery dependency has been removed, only exists for bootstrap from now on.

--- src/GhostController.js
+++ src/GhostController.js
@@ -1,29 +1,30 @@
 async function elementLoaded(placeholder, actual) {
-    $(actual).removeClass('d-none');
+    actual.classList.remove("d-none");
     //$(placeholder).addClass('d-none');
-    $(placeholder).remove();
-
-
+    placeholder.remove();
     //$(placeholder).addClass('successfully-saved');
     
 }
 
 //was non-async
 async function pageActualsInitialize(actuals/*placeholders*/) {
-    for (var i = 0; i < actuals.length; i++) {
-        $(actuals[i]).addClass('d-none');
+    for (let i = 0; i < actuals.length; i++) {
+        //$(actuals[i]).addClass('d-none');
+        actuals[i].classList.add("d-none");
     }
 }
+
 //pageActualsInitialize($('[data-actuality="actual"]'));
 
 
 
 function getActuals(){
-    return $('[data-actuality="actual"]');
+    //return $('[data-actuality="actual"]');
+    return document.querySelectorAll("[data-actuality='actual']");
 }
 
 
 
 function getGhosts() {
-    return $('[data-actuality="ghost"]');
+    return document.querySelectorAll("[data-actuality='ghost']");
 }
--- testEnvironment/testLoaders.js
+++ testEnvironment/testLoaders.js
@@ -2,7 +2,7 @@ async function loadTable1(){
 
 
 
-await sleep(2000);
+//await sleep(2000);
 let data=new dataPack('some data',true);
 return data;
 
